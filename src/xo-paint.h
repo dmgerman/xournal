ebf71977e2b90ac45bbc1c40ee5b2bb6cabaffdf
Denis Auroux <auroux@math.berkeley.edu>
2006-01-26 20:24:14 +0000

    Start CVS repository with release version 0.2

---
+++ src/xo-paint.h
@@ -0,0 +1,19 @@
+void set_cursor_busy(gboolean busy);
+void update_cursor(void);
+
+void create_new_stroke(GdkEvent *event);
+void continue_stroke(GdkEvent *event);
+void finalize_stroke(void);
+
+void do_eraser(GdkEvent *event, double radius, gboolean whole_strokes);
+
+void start_selectrect(GdkEvent *event);
+void finalize_selectrect(void);
+gboolean start_movesel(GdkEvent *event);
+void start_vertspace(GdkEvent *event);
+void continue_movesel(GdkEvent *event);
+void finalize_movesel(void);
+
+void selection_delete(void);
+void selection_to_clip(void);
+void clipboard_paste(void);
