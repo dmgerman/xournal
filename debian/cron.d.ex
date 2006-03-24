edf059223857f28a360e8220d4345fd1d1f5ef35
ivanfrade <>
2006-03-24 17:48:38 +0000

    Debian files to build maemo package

---
+++ debian/cron.d.ex
@@ -0,0 +1,4 @@
+#
+# Regular cron jobs for the xournal package
+#
+0 4	* * *	root	xournal_maintenance
---
+++ debian/menu.ex
@@ -0,0 +1,2 @@
+?package(xournal):needs=X11|text|vc|wm section=Apps/see-menu-manual\
+  title="xournal" command="/usr/bin/xournal"
