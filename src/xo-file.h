7bc3da8971f4155ecffe497a7cc30788db197e5b
Daniel M German <dmg@uvic.ca>
2014-12-09 20:57:42 -0800

    Support for page numbers in Most recently used files.
    - The current page is saved in the .xoj file
    - Under Linux, it is also saved in the gvfs metadata. Precendece is file current page, then gvfs metadata.

--- src/xo-file.h
+++ src/xo-file.h
@@ -12,6 +12,8 @@
  *  You should have received a copy of the GNU General Public License
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+#ifndef XO_FILE_H
+#define XO_FILE_H
 
 #define DEFAULT_SHORTEN_MENUS \
   "optionsProgressiveBG optionsLeftHanded optionsButtonSwitchMapping"
@@ -44,11 +46,6 @@ gboolean init_bgpdf(char *pdfname, gboolean create_pages, int file_domain);
 void bgpdf_create_page_with_bg(int pageno, struct BgPdfPage *bgpg);
 void bgpdf_update_bg(int pageno, struct BgPdfPage *bgpg);
 
-void init_mru(void);
-void update_mru_menu(void);
-void new_mru_entry(char *name);
-void delete_mru_entry(int which);
-void save_mru_list(void);
 
 void init_config_default(void);
 void load_config_from_file(void);
@@ -58,3 +55,5 @@ void autosave_cleanup(GList **list);
 void init_autosave(void);
 gboolean autosave_cb(gpointer is_catchup);
 char *check_for_autosave(char *filename);
+void journal_metadata_page_save(char *filename, int pageno);
+#endif
